c173cac79141942dfecdf6ecea17d04148be0529
Namit Malasi <namitmalasi@gmail.com>
2022-09-10 13:21:33 +0530

    created the sign up page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,8 +1,53 @@
 // styles
 import "./Signup.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign Up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        <span>profile thumbnail:</span>
+        <input type="file" required />
+      </label>
+
+      <button className="btn">Sign Up</button>
+    </form>
+  );
 }
