fc68d405b918a159c6cc2eb310da0e04bcddbb8f
Namit Malasi <namitmalasi@gmail.com>
2022-09-10 13:57:29 +0530

    handling the file inputs

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,9 +8,38 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+
+    if (selected.size > 100000) {
+      setThumbnailError("Image file size must be less than 100kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign Up</h2>
       <label>
         <span>email:</span>
@@ -44,7 +73,8 @@ export default function Signup() {
 
       <label>
         <span>profile thumbnail:</span>
-        <input type="file" required />
+        <input type="file" required onChange={handleFileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
 
       <button className="btn">Sign Up</button>
