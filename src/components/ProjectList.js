fcc3908acf0f96b377e06b6f111bf092096c5977
Namit Malasi <namitmalasi@gmail.com>
2022-09-19 20:20:35 +0530

    creating a project list layout

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ProjectList.css";
+import Avatar from "./Avatar";
 
 function ProjectList({ projects }) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No Projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
