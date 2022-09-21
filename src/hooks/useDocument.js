148de9680633abfce9249a4dc5bd0308a20bddb0
Namit Malasi <namitmalasi@gmail.com>
2022-09-21 11:46:03 +0530

    fetching a project for project details apge

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { projectFirestore } from "../firebase/config";
+
 export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null);
   const [error, setError] = useState(null);
@@ -10,8 +11,12 @@ export const useDocument = (collection, id) => {
 
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
-        setDocument({ ...snapshot.data(), id: snapshot.id });
-        setError(null);
+        if (snapshot.data()) {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        } else {
+          setError("no such document exists");
+        }
       },
       (err) => {
         setError("failed to get document");
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -2,9 +2,26 @@
 import "./Project.css";
 
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
 
 function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
 
 export default Project;
