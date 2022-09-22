4384a2089fe4a79850f02eadfb86f7a0a37c1e61
Namit Malasi <namitmalasi@gmail.com>
2022-09-22 20:17:30 +0530

    listing comments in the project section

--- src/pages/project/ProjectComment.js
+++ src/pages/project/ProjectComment.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
+import Avatar from "../../components/Avatar";
 
 function ProjectComment({ project }) {
   const [newComment, setNewComment] = useState("");
@@ -30,6 +31,26 @@ function ProjectComment({ project }) {
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+
+              <div className="comment-date">
+                <p>date here</p>
+              </div>
+
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
